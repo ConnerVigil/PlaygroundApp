d1f4871ee2e3ef59f9b58c4d3b5647491d1c4dba
connervigil <cjvigil@live.com>
2022-03-21 21:32:09 -0600

    firstcommit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  // const [titleText, setTitleText] = useState("Bird's Nest");
+  // const bodyText = "This is not really a bird nest.";
+
+  // const onPressTitle = () => {
+  //   setTitleText("Bird's Nest [pressed]");
+  // };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/mountain.jpeg')} resizeMode="cover" style={styles.image} >
+
+        <View style={styles.textContainer}>
+          <Text style={styles.titletext}>Welcome to Conner's App</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+            <Button
+              title="Login"
+              color="black"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+        </View>
+
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +34,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  image: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    
+  },
+  buttonContainer: {
+    backgroundColor: '#fff',
+    width: 150,
+    height: 50,
     justifyContent: 'center',
+    bottom: 80,
+    borderRadius: 30,
+  },
+  textContainer: {
+    top: 70,
+  },
+  titletext: {
+    color: '#fff',
+    fontSize: 40,
+    textAlign: 'center',
+    fontWeight: '900',
   },
 });
